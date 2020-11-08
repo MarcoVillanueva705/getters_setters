e9052e82ff3afd0a7a25ba7d168fdc35af91686f
marco villanueva <villanuevamarco98@gmail.com>
2020-11-07 21:00:54 -0700

    research get return

--- getters_setters/Program.cs
+++ getters_setters/Program.cs
@@ -2,13 +2,27 @@
 
 namespace getters_setters
 {
+    class Person
+    {
+        private string name; // field
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //.WriteLine("Hello World!");
             //code will go here for Sunday
             //will start with get set properties
+            Person myObj = new Person();
+            //myObj.Name = "Liam";
+            // Console.WriteLine(myObj.Name);
+            Console.WriteLine(myObj);
         }
     }
 }
