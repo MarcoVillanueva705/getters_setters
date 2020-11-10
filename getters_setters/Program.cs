b59e84230e86ae9a26fda8fd910476eb39fa4c6f
marco villanueva <villanuevamarco98@gmail.com>
2020-11-09 20:47:47 -0700

    why encapsulation

--- getters_setters/Program.cs
+++ getters_setters/Program.cs
@@ -23,7 +23,7 @@ namespace getters_setters
             //}
 
             //but if you also have 'set', you can assign a new value to 'Person.Name'
-            //belwo would return 'Liam'
+            //below would return 'Liam'
             set { name = value; }
         }
 
@@ -41,5 +41,13 @@ namespace getters_setters
             // Console.WriteLine(myObj.Name);
             Console.WriteLine(myObj.Name);
         }
+        //Why Encapsulation?
+        //Better control of class members (reduce the possibility 
+        //of yourself (or others) to mess up the code)
+        //Fields can be made read-only (if you only use the get method), 
+        //or write-only (if you only use the set method)
+        //Flexible: the programmer can change one part of the code without affecting other parts
+        //Increased security of data
+
     }
 }
